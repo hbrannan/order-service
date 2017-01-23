3ca698842ff7ad3db0da7dc414efc0f8da3e1cb4
Hannah_Brannan <hannahbrannan@gmail.com>
2017-01-23 13:25:41 +0100

    feat (app.js) add CORS headers

--- server/app.js
+++ server/app.js
@@ -2,10 +2,24 @@ var express = require('express');
 var utils = require('./utils.js');
 var path = require('path');
 var port = process.env.PORT || 5000;
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
 
 var app = express();
-express.static(path.join(__dirname, 'client'))
 
+app.use(allowCrossDomain);
+express.static(path.join(__dirname, 'client'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../client', 'index.html'));
