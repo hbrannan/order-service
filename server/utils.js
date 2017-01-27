eabae91a229127a72cb8b293bb01e33c15bc35c1
Hannah_Brannan <hannahbrannan@gmail.com>
2017-01-27 09:31:34 +0100

    rever (utils) Waitlist

--- server/utils.js
+++ server/utils.js
@@ -89,7 +89,7 @@ var enrollUser = function (id, prepType, res) {
   console.log('enrollUser is called');
 
   var expiryDate = null;
-  if (prepType === 'Premium' || prepType === 'Premium') {
+  if (prepType === 'Premium' || prepType === 'Waitlist') {
     expiryDate = new Date();
     expiryDate.setFullYear(expiryDate.getFullYear() + 2);
   }
